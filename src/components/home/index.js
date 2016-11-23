17b20949c213a8e732c793d6a752be2066300539
Tony Petty <tpetty@synacor.com>
2016-11-23 13:51:37 -0500

    starting

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,8 +5,9 @@ export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
+				<h1>Welcome</h1>
+				<p><a href="mailto:SPGWhistler@gmail.com">Contact Me</a></p>
+				<p><a href="https://www.linkedin.com/in/anthony-petty-2a88b050">LinkedIn</a></p>
 			</div>
 		);
 	}
